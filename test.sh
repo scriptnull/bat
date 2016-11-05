953a7cbcd67ac6259c14d92cba8458dfd9ec539e
Avi Cavale <avi@shippable.com>
2016-11-05 10:27:09 -0700

    extract params

--- test.sh
+++ test.sh
@@ -10,9 +10,11 @@ setupTestEnv() {
 #  popd
 
   pushd /build/IN/$RES_PARAMS
-  jq -r '.version.propertyBag.params.secure' params > avi.txt
-  . avi.txt
-  echo $API_URL
+  cat params | jq -r '.version.propertyBag.params.secure'
+  jq -r '.version.propertyBag.params.secure' params
+  #params > avi.txt
+  #. avi.txt
+  #echo $API_URL
   popd
   echo "Completed Testing Env setup" $RES_REPO
 }
