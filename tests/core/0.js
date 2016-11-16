b52c3da7f266251bf2a0da34f13ce578115302a7
deepikasl <deepika@shippable.com>
2016-11-16 15:52:41 +0530

    if test fails create a issue

--- tests/core/0.js
+++ tests/core/0.js
@@ -38,18 +38,59 @@ describe(util.format('%s1 - %s', testSuiteNum, testSuiteDesc),
 
     it('Get Accounts',
       function (done) {
+        this.timeout(15000);
+        setTimeout(done, 15000);
         var apiToken = util.format('apiToken %s', config.apiToken);
         chai.request(config.apiUrl)
           .get('/accounts')
           .set('Authorization', apiToken)
           .end(function(err, res){
-             logger.debug("res is::", util.inspect(res.body,{depth:null}));
-             if (res.status<200 || res.status>=299)
-               logger.warn("status is::",res.status);
-             return done();
+            if (err) {
+              var bag = {
+                testSuite: 'Get Accounts',
+                error: err
+              }
+              async.series([
+                  _createIssue.bind(null, bag)
+                ],
+                function (err) {
+                  if (err) {
+                    logger.warn('Failed');
+                    return done();
+                  }
+                  else {
+                    logger.debug('Issue Created');
+                    return done();
+                  }
+                }
+              );
+            } else {
+              logger.debug("res is::", util.inspect(res.body,{depth:null}));
+              if (res.status<200 || res.status>=299)
+                logger.warn("status is::",res.status);
+              return done();
+            }
           }
         );
       }
     );
   }
 );
+
+function _createIssue(bag,next) {
+  var githubAdapter = new adapter(config.githubToken, config.githubUrl);
+  var title = util.format('Failed test case %s', bag.testSuite);
+  var body = util.format('Failed with error: %s', bag.error);
+  var data = {
+    title: title,
+    body: body
+  }
+  githubAdapter.pushRespositoryIssue('deepikasl', 'VT1', data,
+    function(err, res) {
+      logger.debug("response is::",res.status);
+      if (err)
+        logger.warn("Creating Issue failed with error: ", err);
+      return next();
+    }
+  );
+}
