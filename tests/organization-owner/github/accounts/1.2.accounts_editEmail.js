33ba2fb0ed5d08c906a9ee5f8c04e8705fbe9406
deepikasl <deepika@shippable.com>
2016-11-24 18:21:33 +0530

    add should

--- tests/organization-owner/github/accounts/1.2.accounts_editEmail.js
+++ tests/organization-owner/github/accounts/1.2.accounts_editEmail.js
@@ -9,7 +9,6 @@ var testSuiteNum = '1.';
 var testSuiteDesc = 'Edit email with valid email address';
 var adapter = require('../../../../_common/shippable/github/Adapter.js');
 var Shippable = require('../../../../_common/shippable/Adapter.js');
-var should = require('should');
 
 var testSuite = util.format('%s2.accounts_editEmail - %s', testSuiteNum,
                   testSuiteDesc);
@@ -523,8 +522,7 @@ describe('Edit email with valid email address',
                       }
                     }
                   );
-                  var errMsg = 'No error';
-                  errMsg.should.not.be.equal("No error");
+                  logger.error('Missing @ sign and domain testCase failed');
                   return done();
                 }
               }
