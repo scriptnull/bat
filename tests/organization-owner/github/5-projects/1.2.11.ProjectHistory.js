95743c46ca248c8e86a8f6538aa13aab032e4e93
Riturajcse <riturajcse1994@gmail.com>
2017-02-20 14:34:21 +0530

    adds projects/:id/history page test case

---
+++ tests/organization-owner/github/5-projects/1.2.11.ProjectHistory.js
@@ -0,0 +1,197 @@
+'use strict';
+
+var mocha = require('mocha');
+var nconf = require('nconf');
+var chai = require('chai');
+var _ = require('underscore');
+var assert = chai.assert;
+var testSuiteNum = '1.';
+var testSuiteDesc = 'Project History';
+var adapter = require('../../../../_common/shippable/github/Adapter.js');
+var Shippable = require('../../../../_common/shippable/Adapter.js');
+
+var testSuite = util.format('%s2 - %s', testSuiteNum,
+  testSuiteDesc);
+
+var isTestFailed = false;
+var testCaseErrors = [];
+var projectId = '';
+var runId = '';
+var shippable;
+var run = {};
+
+describe('Project History',
+  function() {
+
+    describe(testSuite,
+      function () {
+
+        it('Get ProjectsById',
+          function (done) {
+            this.timeout(0);
+            var pathToJson = process.cwd() + '/config.json';
+            nconf.argv().env().file({file: pathToJson});
+            nconf.load();
+            projectId = nconf.get('shiptest-GITHUB_ORG_1:projectId');
+            shippable = new Shippable(config.apiToken);
+            shippable.getProjectById(projectId,
+              function (err, proj) {
+                if (err) {
+                  isTestFailed = true;
+                  var testCase =
+                    util.format(
+                      '\n - [ ] %s Failed to get project for projectId:' +
+                      ' %s failed with error: %s', testSuiteDesc, projectId,
+                      err);
+                  testCaseErrors.push(testCase);
+                  assert.equal(err, null);
+                  return done();
+                } else {
+                  console.log('Get ProjectsById successful for : ' + projectId);
+                  projectId = proj.id;
+                  return done();
+                }
+              }
+            );
+          }
+        );
+
+        it('Get Runs',
+          function (done) {
+            this.timeout(0);
+            var query = util.format('type=ci&projectIds=%s&status=complete' +
+              '&limit=21&sortBy=createdAt,runNumber&sortOrder=-1,-1',
+              projectId);
+            shippable.getRuns(query,
+              function(err, runs) {
+                if (err) {
+                  isTestFailed = true;
+                  var testCase =
+                    util.format(
+                      '\n- [ ] %s: Get runs, failed with error: %s',
+                      testSuiteDesc, err);
+                  testCaseErrors.push(testCase);
+                  assert.equal(err, null);
+                  return done();
+                } else {
+                  if (runs.status<200 || runs.status>=299)
+                    logger.warn('status is::',runs.status);
+                  run = _.first(runs);
+                  return done();
+                }
+              }
+            );
+          }
+        );
+
+        it('Trigger new build request',
+          function (done) {
+            this.timeout(0);
+            var payload = {
+              runId: run.id
+            };
+            shippable.triggerNewBuildByProjectId(run.projectId, payload,
+              function (err, run) {
+                if (err) {
+                  isTestFailed = true;
+                  var testCase =
+                    util.format(
+                      '\n - [ ] %s Trigger new build request for projectId:' +
+                      ' %s failed with error: %s', testSuiteDesc, run.projectId,
+                      err);
+                  testCaseErrors.push(testCase);
+                  assert.equal(err, null);
+                  return done();
+                } else {
+                  console.log('Triggered new build with runId: ' + run.runId);
+                  return done();
+                }
+              }
+            );
+          }
+        );
+
+        it('Get RunsById',
+          function (done) {
+            this.timeout(0);
+            var query = util.format('runIds=' + run.id);
+            shippable.getRuns(query,
+              function(err, runs) {
+                if (err) {
+                  isTestFailed = true;
+                  var testCase =
+                    util.format(
+                      '\n- [ ] %s: Get runs, failed with error: %s',
+                      testSuiteDesc, err);
+                  testCaseErrors.push(testCase);
+                  assert.equal(err, null);
+                  return done();
+                } else {
+                  if (runs.status<200 || runs.status>=299)
+                    logger.warn('status is::',runs.status);
+                  run = _.first(runs);
+                  runId = run.id;
+                  return done();
+                }
+              }
+            );
+          }
+        );
+
+        it('Delete Run',
+          function (done) {
+            this.timeout(0);
+            shippable.deleteRunById(runId,
+              function (err) {
+                if (err) {
+                  isTestFailed = true;
+                  var testCase =
+                    util.format(
+                      '\n - [ ] %s delete run failed with error: %s' +
+                      testSuiteDesc, err);
+                  testCaseErrors.push(testCase);
+                  assert.equal(err, null);
+                  return done();
+                } else {
+                  console.log('Run Deleted');
+                  return done();
+                }
+              }
+            );
+          }
+        );
+      }
+    );
+
+    describe('Should run after above test suites',
+      function () {
+
+        it('Creating Github Issue if test cases failed',
+          function (done) {
+            this.timeout(0);
+            if (isTestFailed) {
+              var githubAdapter =
+                new adapter(config.githubToken, config.githubUrl);
+              var title = util.format('Failed test suite %s', testSuite);
+              var body = util.format(
+                'Failed test cases are:\n%s',testCaseErrors);
+              var data = {
+                title: title,
+                body: body
+              };
+              githubAdapter.pushRespositoryIssue('deepikasl', 'VT1', data,
+                function(err, res) {
+                  if (err)
+                    logger.warn('Creating Issue failed with error: ', err);
+                  return done();
+                }
+              );
+            } else {
+              return done();
+            }
+          }
+        );
+      }
+    );
+  }
+);
