66396ba79d11b45179b90175cf900fc1bbbdf17e
Riturajcse <riturajcse1994@gmail.com>
2017-02-20 15:02:07 +0530

    uses run.id instead of run.runId

--- tests/organization-owner/github/5-projects/1.2.11.ProjectHistory.js
+++ tests/organization-owner/github/5-projects/1.2.11.ProjectHistory.js
@@ -103,7 +103,7 @@ describe('Project History',
                   assert.equal(err, null);
                   return done();
                 } else {
-                  console.log('Triggered new build with runId: ' + run.runId);
+                  console.log('Triggered new build with runId: ' + run.id);
                   return done();
                 }
               }
